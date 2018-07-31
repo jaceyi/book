7ddbff9dab647a9fa9c4478612b1d3f562c0beb2
yijinchun@outlook.com <yijinchun@outlook.com>
2018-07-31 09:09:55 +0800

    优化 scroll

--- src/router.js
+++ src/router.js
@@ -8,14 +8,17 @@ export default function() {
   const bgUrl = `url(//yijic.com/public/images/bg/${Math.floor(Math.random() * 13 + 1)}.jpg)`;
 
   return(
-    <div className={'container'} style={{backgroundImage: bgUrl}}>
-      <HashRouter>
-        <Switch>
-          <Route path={'/'} exact component={Index}/>
-          <Route path={'/book/:id'} exact component={Book}/>
-          <Route path={'/book/:id/chapter/:link'} exact component={Chapter}/>
-        </Switch>
-      </HashRouter>
+    <div className={'container'}>
+      <div className={'container__bg'} style={{backgroundImage: bgUrl}}/>
+      <div className={'container__main'}>
+        <HashRouter>
+          <Switch>
+            <Route path={'/'} exact component={Index}/>
+            <Route path={'/book/:id'} exact component={Book}/>
+            <Route path={'/book/:id/chapter/:link'} exact component={Chapter}/>
+          </Switch>
+        </HashRouter>
+      </div>
     </div>
   )
 }
